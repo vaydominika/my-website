2937c3c9c8eb6b9355ab140f6f0de10270f99deb
vaydominika <vaydominika@gmail.com>
2024-08-02 22:49:15 +0200

    Added Animations

---
+++ src/js/ScrollToTopButton.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import "../css/ScrollToTopButton.css";
+
+const ScrollToTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > window.innerHeight / 2) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <button
+      className={`scroll-to-top ${isVisible ? "show" : ""}`}
+      onClick={scrollToTop}
+    >
+      ↑
+    </button>
+  );
+};
+
+export default ScrollToTopButton;
